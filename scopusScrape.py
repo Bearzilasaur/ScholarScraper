7e3b56abac5d9fe218fc1b857101a0c06e6bf611
Bearzilasaur <mitchell.baum@gmail.com>
2019-05-13 16:17:43 +1000

    Basic Scopus API search for 'query'

---
+++ scopusScrape.py
@@ -0,0 +1,26 @@
+from scopus import ScopusSearch
+import pandas as pd 
+import json
+
+#Documentation can be found at https://scopus.readthedocs.io/en/stable/
+#Author: Mitchell Baum
+
+def scopArts(n=10):
+    query   = input('\nPlease input your query:\n')
+    api     = input('\nPlease input your api:\n')
+
+    queryApi = query.replace('', '+') + format('&apiKey={}', api)
+    srch = ScopusSearch(queryApi, count = n)
+    jsrch = json.loads(srch)
+
+    print(jsrch)
+
+    return jsrch
+
+
+
+
+
+
+
+
