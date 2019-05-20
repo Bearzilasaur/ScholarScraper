7f4fc021ff01a950dc914cd3d38a51ccd0c7a211
Bearzilasaur <mitchell.baum@gmail.com>
2019-05-20 16:07:26 +1000

    In Development

---
+++ ScholScrapers.py
@@ -0,0 +1,113 @@
+'''
+---------------------------------------------------------------------
+Scrapes google scholar search results and returns a numpy array of the 
+first 10 results with the relevent information (authors, title, absract 
+etc.)
+=============================================================================
+Author: Mitchell Baum
+=============================================================================
+'''
+
+'''DOCSTRING note triple apostrophe's are docstrings and are generally
+used for explaining what a piece of code does. Comments (#) should be 
+used to explain how something works or what it is doing. Need to go 
+through and edit all docstring into comments. '''
+
+
+
+'''Importing packages'''
+import requests
+from bs4 import BeautifulSoup
+import numpy
+import time
+import random
+import re
+
+'''importing classes from ScholarClasses.py'''
+from ScholarClasses import article, bsSchol, url, UniqueDict
+
+
+
+'''Takes a dict as input (?or two tuples) and scrapes google scholar for 
+each key:value pair. Intended use: pass an Embayment name as key and 
+keywords as values. Output in numpy format. '''
+#TODO: needs testing!!!
+def scholarSpider(embDict):
+    
+    if type(embDict) != dict:
+        raise AttributeError('Input must be dictionary')
+    
+    spiderOut = numpy.array([[
+            "id",
+            "Embayment",
+            "Article Title",
+            "Author/s",
+            "Abstract",
+            "Citations",
+            "Link"]])
+    
+    for embayment in embDict:
+        embayment_query     = embayment
+        extra_query_terms   = embDict[embayment]
+        query               = embayment_query.strip() + " " + extra_query_terms.strip()
+        search_url          = url(query)
+        search_bsObj        = bsSchol(search_url.url)
+
+        #used to create a unique id within the numpy table. 
+        number = 0
+
+        for i in search_bsObj.results:
+            entry = article(
+                number,
+                embayment_query,
+                i)
+            number += number
+
+            spiderOut = numpy.append(spiderOut, entry.as_array(), axis=0)
+        
+        '''Waits a random amount of time before collecting a new html'''
+        time.sleep(random.randint(1,10))
+    return spiderOut
+
+
+
+'''Takes user input and returns a numpy array with the releventa info
+from the first 10 results'''
+def scholarScrape():
+
+    '''Asking for user input for scholar search'''
+    embayment_query     = input("\n\nSearch:\n")
+    extra_query_terms   = input("\n\nExtra Search Terms:\n")
+    query               = embayment_query + " " + extra_query_terms
+    search_url          = url(query)
+    search_bsObj        = bsSchol(search_url.url)
+
+    article_array = numpy.array([[
+        "id",
+        "Embayment",
+        "Article Title",
+        "Author/s",
+        "Year",
+        "Publication",
+        "Abstract",
+        "Citations",
+        "Link"]])
+
+    
+    number = 0
+
+    for i in search_bsObj.results:
+        entry = article(
+            number,
+            embayment_query,
+            i)
+        number += number
+
+        article_array = numpy.append(article_array, entry.as_array(), axis=0)
+    
+    return article_array
+        
+    
+    
+    
+        
---
+++ ScholarClasses.py
@@ -0,0 +1,103 @@
+'''======= Classes used in for the Scholar Scraper functions========='''
+
+'''Author: Mitchell Baum'''
+'''=================================================================='''
+
+'''Article class for storing scraped information. Contains as_list 
+method which returns the information as a list and as_dict which 
+returns a dict'''
+
+'''TODO: DOCSTRING note triple apostrophe's are docstrings and are generally
+used for explaining what a piece of code does. Comments (#) should be 
+used to explain how something works or what it is doing. Need to go 
+through and edit all docstring into comments. '''
+
+'''required packages'''
+from bs4 import BeautifulSoup
+import numpy
+import requests
+import re
+
+class article:
+    def __init__(self, no, embayment, bsObj):
+        
+        
+        self.no         = no
+        self.embayment  = embayment
+        self.title      = bsObj.find('h3', {'class':'gs_rt'}).text
+        self.auth, self.dateyear= bsObj.find('div', {'class':'gs_a'}).text.split('-')[0:2]
+        #TODO: ^^^ This method falls apart when any author has a '-' in 
+        #their name. maybe use ' - ' instead as whitespace within 
+        #hyphenated names is unlikely. 
+        self.abstract   = bsObj.find('div', {'class':'gs_rs'}).text
+        self.citations  = bsObj.find('div', {'class': 'gs_fl'}).find(text=re.compile("^Cited by"))
+        self.link       = bsObj.find('h3', {'class':'gs_rt'}).find('a', href = True)['href']
+    
+    '''Method which returns the information as a list with all the 
+    information stored in the class'''
+    def as_array(self):
+        x = numpy.array([[
+            self.no,
+            self.embayment,
+            self.title,
+            self.auth,
+            self.abstract,
+            self.dateyear,
+            self.link
+        ]])
+        return x
+
+    
+    '''Converts article information to a dict and returns it'''
+
+    def dictionary(self):
+        x = dict({  'Id':self.no,
+        'Embayment':self.embayment,
+        'Article':self.title,
+        'Author/s':self.auth,
+        'Abstract':self.abstract,
+        'Year':self.dateyear,
+        'Link':self.link
+        })
+
+        return x 
+
+'''Class which sanitizes dictionary input to make sure that all 
+keys(embayments) are unique. 
+Taken from: https://stackoverflow.com/questions/5947950/how-can-i-force-a-dictionary-in-python-to-only-have-unique-keys'''
+class UniqueDict(dict):
+    def __setitem__(self, key, value):
+        if key not in self:
+            dict.__setitem__(self, key, value)
+        else:
+            raise KeyError("Key already exists")
+
+
+'''A url class for converting user input into a url which can be 
+requested using requests or urllib etc.'''
+class url:
+
+    def __init__(self, query, url="https://scholar.google.com.au/scholar?start={}&q="):
+        
+        '''Formats the input embayment and search terms to create the 
+        url for the search''' 
+        queryTerms = query.strip().split(' ')
+        separator ='+'
+        prefix = url.format('00')
+
+        self.prefix = prefix
+        self.suffix = "&hl=en&as_sdt=0,5"
+        self.terms  = separator.join(queryTerms)
+        self.url    = self.prefix + self.terms + self.suffix
+
+
+'''Class which takes in a url as an argument, requests it using requests, 
+then converts it into a beautiful soup object, and returns the 
+<body></body> section'''
+class bsSchol:
+    
+    def __init__(self, url):
+        self.html = requests.get(url, headers = {"User-Agent":"Mozilla/5.0"})
+        self.soup = BeautifulSoup(self.html.content, "html.parser")
+        self.body = self.soup.find("div", {"id":"gs_res_ccl_mid"})
+        self.results = self.body.findAll("div", {"class":"gs_ri"})
