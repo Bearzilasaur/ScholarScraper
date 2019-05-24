f7a9750ce6ec658513de340cab9ef933fd97cad0
Bearzilasaur <mitchell.baum@gmail.com>
2019-05-24 15:35:38 +1000

    SQL interface functions

--- s2sql.py
+++ s2sql.py
@@ -11,4 +11,19 @@ def sqlsend(nparr, db, table):
 
     df.to_sql(table, con=conn, if_exists='fail')
 
+def sqlappend(nparr, db, table):
+    df = pd.DataFrame(nparr)
+    conn = sql.connect("{}.db".format(db))
+
+    #stores the target table from sql
+    sqltable = pd.read_sql_table(table_name=tabe, con=conn)
+
+    #appends the input table to target table using pandas
+    combtable = sqltable.append(nparr)
+
+    #exports the combined table back to sql
+    combtable.to_sql(table, con = conn, if_exists='replace')
+
+
+
     
