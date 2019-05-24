0a260a23df7eda5362714994026027c04dbf5909
Bearzilasaur <mitchell.baum@gmail.com>
2019-05-24 15:42:06 +1000

    Updating ScopusScraper

--- ScopusScraper.py
+++ ScopusScraper.py
@@ -1,13 +1,16 @@
 from scopus import ScopusSearch
-import pandas as pd 
 import json
 
 #Documentation can be found at https://scopus.readthedocs.io/en/stable/
 #Author: Mitchell Baum
 
-def scopArts(n=10):
+#TODO: Create scopus search functions for retrieval of basic information
+#and for import of abstracts with scopArts and scopAbs. scopQuery used 
+#to interface with scopus api using user input
+
+def scopQuery(n=10, api="aba012f82a0a994632b6c8253eba6c91"):
     query   = input('\nPlease input your query:\n')
-    api     = input('\nPlease input your api key:\n')
+    #api     = input('\nPlease input your api key:\n') <- deprecated
 
     queryApi = query.replace('', '+') + format('&apiKey={}', api)
     srch = ScopusSearch(queryApi, count = n)
@@ -17,7 +20,7 @@ def scopArts(n=10):
 
     return jsrch
 
-
+def scopAbs(n=10)
 
 
 
